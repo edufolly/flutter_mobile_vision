fdbe15a46270d8c2d0050aa7bbe155399c036b15
Eduardo Folly <edufolly@gmail.com>
2020-04-25 20:49:48 -0300

    Removing flutter_plugin_android_lifecycle.

--- android/src/main/java/io/github/edufolly/flutter_mobile_vision/FlutterMobileVisionPlugin.java
+++ android/src/main/java/io/github/edufolly/flutter_mobile_vision/FlutterMobileVisionPlugin.java
@@ -1,20 +1,14 @@
 package io.github.edufolly.flutter_mobile_vision;
 
 import android.app.Activity;
-import android.app.Application;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.DefaultLifecycleObserver;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleOwner;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.embedding.engine.plugins.lifecycle.FlutterLifecycleAdapter;
 
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
@@ -30,98 +24,19 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  */
 public class FlutterMobileVisionPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
-    private class LifeCycleObserver
-            implements Application.ActivityLifecycleCallbacks, DefaultLifecycleObserver {
-        private final Activity thisActivity;
-
-        LifeCycleObserver(Activity activity) {
-            this.thisActivity = activity;
-        }
-
-        @Override
-        public void onCreate(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onStart(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onResume(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onPause(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onStop(@NonNull LifecycleOwner owner) {
-            onActivityStopped(thisActivity);
-        }
-
-        @Override
-        public void onDestroy(@NonNull LifecycleOwner owner) {
-            onActivityDestroyed(thisActivity);
-        }
-
-        @Override
-        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        }
-
-        @Override
-        public void onActivityStarted(Activity activity) {
-        }
-
-        @Override
-        public void onActivityResumed(Activity activity) {
-        }
-
-        @Override
-        public void onActivityPaused(Activity activity) {
-        }
-
-        @Override
-        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-        }
-
-        @Override
-        public void onActivityDestroyed(Activity activity) {
-            if (thisActivity == activity && activity.getApplicationContext() != null) {
-                ((Application) activity.getApplicationContext())
-                        .unregisterActivityLifecycleCallbacks(
-                                this); // Use getApplicationContext() to avoid casting failures
-            }
-        }
-
-        @Override
-        public void onActivityStopped(Activity activity) {
-//            if (thisActivity == activity) {
-//                 delegate.saveStateBeforeResult();
-//            }
-        }
-    }
-
     private static final String CHANNEL = "flutter_mobile_vision";
 
     private MethodChannel channel;
     private FlutterMobileVisionDelegate delegate;
     private FlutterPluginBinding pluginBinding;
     private ActivityPluginBinding activityBinding;
-    private Application application;
     private Activity activity;
-    // This is null when not using v2 embedding;
-    private Lifecycle lifecycle;
-    private LifeCycleObserver observer;
 
     @SuppressWarnings("unused")
     public static void registerWith(Registrar registrar) {
         Activity activity = registrar.activity();
-        Application application = null;
-        if (registrar.context() != null) {
-            application = (Application) (registrar.context().getApplicationContext());
-        }
         FlutterMobileVisionPlugin plugin = new FlutterMobileVisionPlugin();
-        plugin.setup(registrar.messenger(), application, activity, registrar, null);
+        plugin.setup(registrar.messenger(), activity, registrar, null);
     }
 
 
@@ -140,7 +55,6 @@ public class FlutterMobileVisionPlugin implements FlutterPlugin, MethodCallHandl
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         activityBinding = binding;
         setup(pluginBinding.getBinaryMessenger(),
-                (Application) pluginBinding.getApplicationContext(),
                 activityBinding.getActivity(),
                 null,
                 activityBinding);
@@ -163,27 +77,22 @@ public class FlutterMobileVisionPlugin implements FlutterPlugin, MethodCallHandl
 
     private void setup(
             final BinaryMessenger messenger,
-            final Application application,
+
             final Activity activity,
             final PluginRegistry.Registrar registrar,
             final ActivityPluginBinding activityBinding) {
         this.activity = activity;
-        this.application = application;
         this.delegate = new FlutterMobileVisionDelegate(activity);
         channel = new MethodChannel(messenger, CHANNEL);
         channel.setMethodCallHandler(this);
-        observer = new LifeCycleObserver(activity);
         if (registrar != null) {
             // V1 embedding setup for activity listeners.
-            application.registerActivityLifecycleCallbacks(observer);
             registrar.addActivityResultListener(delegate);
             registrar.addRequestPermissionsResultListener(delegate);
         } else {
             // V2 embedding setup for activity listeners.
             activityBinding.addActivityResultListener(delegate);
             activityBinding.addRequestPermissionsResultListener(delegate);
-            lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(activityBinding);
-            lifecycle.addObserver(observer);
         }
     }
 
@@ -191,13 +100,9 @@ public class FlutterMobileVisionPlugin implements FlutterPlugin, MethodCallHandl
         activityBinding.removeActivityResultListener(delegate);
         activityBinding.removeRequestPermissionsResultListener(delegate);
         activityBinding = null;
-        lifecycle.removeObserver(observer);
-        lifecycle = null;
         delegate = null;
         channel.setMethodCallHandler(null);
         channel = null;
-        application.unregisterActivityLifecycleCallbacks(observer);
-        application = null;
     }
 
     // MethodChannel.Result wrapper that responds on the platform thread.
